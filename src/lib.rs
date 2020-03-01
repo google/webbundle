dd33d2bbfe017767e4a7d7402d980561b2e0ccdc
kwangin.jung <inylove82@gmail.com>
2020-03-01 10:40:25 +0900

    Add cli guide
    Add cli guide in document

--- src/lib.rs
+++ src/lib.rs
@@ -46,6 +46,19 @@
 //! bundle.write_to(write)?;
 //! # Result::Ok::<(), anyhow::Error>(())
 //! ```
+//! 
+//! ## 'webbundle' command line tool
+//! 
+//! ### create
+//! ```no_run
+//! $ webbundle create -b "https://example.com/" -p "https://example.com/foo/index.html" example.wbn foo
+//! ```
+//! 
+//! ### dump
+//! ```no_run
+//! $ webbundle dump ./example.wbn
+//! ```
+//! 
 
 mod builder;
 mod bundle;
