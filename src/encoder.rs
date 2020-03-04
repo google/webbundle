96f963ef8b6111d41218eb3f3c9148e1878b4487
Hayato Ito <hayato@google.com>
2020-03-04 21:51:03 +0900

    Sort map keys in encoding

--- src/encoder.rs
+++ src/encoder.rs
@@ -192,20 +192,31 @@ fn encode_response_section(exchanges: &[Exchange]) -> Result<(Vec<u8>, Vec<Respo
 }
 
 fn encode_index_section(response_locations: &[ResponseLocation]) -> Result<Vec<u8>> {
-    let mut bytes = Vec::new();
-    let mut se = Serializer::new(&mut bytes);
-
-    se.write_map(Len::Len(response_locations.len() as u64))?;
+    // Map keys must be sorted.
+    // See [3.9. Canonical CBOR](https://tools.ietf.org/html/rfc7049#section-3.9)
+    let mut map = std::collections::BTreeMap::<Vec<u8>, Vec<u8>>::new();
 
     for response_location in response_locations {
-        se.write_text(response_location.uri.to_string())?;
-        se.write_array(Len::Len(3))?;
+        let mut key = Serializer::new(Vec::new());
+        key.write_text(response_location.uri.to_string())?;
+
+        let mut value = Serializer::new(Vec::new());
+        value.write_array(Len::Len(3))?;
         // TODO: Support variants.
-        se.write_bytes(b"")?;
-        se.write_unsigned_integer(response_location.offset as u64)?;
-        se.write_unsigned_integer(response_location.length as u64)?;
+        value.write_bytes(b"")?;
+        value.write_unsigned_integer(response_location.offset as u64)?;
+        value.write_unsigned_integer(response_location.length as u64)?;
+
+        map.insert(key.finalize(), value.finalize());
     }
-    Ok(bytes)
+
+    let mut se = Serializer::new(Vec::new());
+    se.write_map(Len::Len(response_locations.len() as u64))?;
+    for (key, value) in map {
+        se.write_raw_bytes(&key)?;
+        se.write_raw_bytes(&value)?;
+    }
+    Ok(se.finalize())
 }
 
 fn encode_section_lengths(sections: &[Section]) -> Result<Vec<u8>> {
@@ -221,19 +232,31 @@ fn encode_section_lengths(sections: &[Section]) -> Result<Vec<u8>> {
 }
 
 fn encode_headers(response: &Response) -> Result<Vec<u8>> {
-    let mut bytes = Vec::new();
-    let mut se = Serializer::new(&mut bytes);
-
-    se.write_map(Len::Len((response.headers().len() + 1) as u64))?;
+    // Map keys must be sorted.
+    // See [3.9. Canonical CBOR](https://tools.ietf.org/html/rfc7049#section-3.9)
+    let mut map = std::collections::BTreeMap::<Vec<u8>, Vec<u8>>::new();
 
     // Write status
-    se.write_bytes(b":status")?;
-    se.write_bytes(response.status().as_u16().to_string().as_bytes())?;
+    let mut key = Serializer::new(Vec::new());
+    key.write_bytes(b":status")?;
+    let mut value = Serializer::new(Vec::new());
+    value.write_bytes(response.status().as_u16().to_string().as_bytes())?;
+    map.insert(key.finalize(), value.finalize());
 
     // Write headers
-    for (name, value) in response.headers() {
-        se.write_bytes(name.as_str().as_bytes())?;
-        se.write_bytes(value.to_str()?.as_bytes())?;
+    for (header_name, header_value) in response.headers() {
+        let mut key = Serializer::new(Vec::new());
+        key.write_bytes(header_name.as_str().as_bytes())?;
+        let mut value = Serializer::new(Vec::new());
+        value.write_bytes(header_value.to_str()?.as_bytes())?;
+        map.insert(key.finalize(), value.finalize());
     }
-    Ok(bytes)
+
+    let mut se = Serializer::new(Vec::new());
+    se.write_map(Len::Len(map.len() as u64))?;
+    for (key, value) in map {
+        se.write_raw_bytes(&key)?;
+        se.write_raw_bytes(&value)?;
+    }
+    Ok(se.finalize())
 }
